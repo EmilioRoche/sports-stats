cb931ea35a98f09e48450e29081067912ca40da3
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-08-30 19:31:14 -0600

    added home nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Sports from './components/Sports';
+import Nav from './components/Nav';
 
 function App(){
   return ( 
     <Router>
       <div className="App">
         <header className="App-header">
-            <Routes>
+          <Nav>
+          <Routes>
             <Route path="/sports-stats" index element={<Sports />} />
             </Routes>
+          </Nav>
         </header>
       </div>
     </Router>
