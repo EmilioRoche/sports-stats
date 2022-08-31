53a450554edb5b7890286b199bda35daae0dd104
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-08-31 17:47:39 -0600

    introduced bootstrap design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Sports from './components/Sports';
 import Nav from './components/Nav';
+import Content from './components/Content';
 
 function App(){
   return ( 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Nav(){
     return(
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark top">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navMainMenu" class="navbar-collapse collapse">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <div class="container px-5">
+            <a class="navbar-brand" href="#!">Sports Stats</a>
+        </div>
+
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div id="navMainMenu" class="navbar-collapse collapse">
                 <div class="navbar-nav ml-auto">
                     <Link to='/sports-stats' className="nav-item nav-link active">Home</Link>
                 </div>
             </div>
+        </div>
         </nav>
     )
 }
