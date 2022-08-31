ecd240f5b68edba448565dc0c0c100b2f59cd8af
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-08-30 19:14:35 -0600

    added page with content

---
+++ src/components/Sports.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sports() {
+    return(
+        <section>
+            <div class="container-fluid">
+                <h1 class="mt-5">Welcome</h1>
+            </div>
+        </section>        
+    );
+}
+export default Sports;
