a013004782231e78a9995aa25b40efe71db01435
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-11 20:15:15 -0600

    finally got the text to show up the way I wanted, just need to style it

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,30 +3,43 @@ import MainTextBox from './MainTextBox';
 import axios from 'axios';
 
 function Content() {
-    async function load() {
-        axios.get("https://stats-sports-api.herokuapp.com").then(response => {
-            const sportsData = response.data.data;
-            console.log(sportsData);
-            setSport(sportsData);
-        });
-    }
-    /*
-    function loadContent() {
-        fetch("https://stats-sports-api.herokuapp.com", {
-            mode: 'no-cors',
-        })
-        .then((response) => response.json()).then((data) => {
-            setSport(data.sport);
-        }
-    )}
-    */
-    const [sport, setSport] = useState("");
-    useEffect( () => {load()}, []);
+    const [sports, setSports] = useState([]);
+    useEffect( () => {load();}, []);
+
+    const useSports = sports.map((sport) => {
+        return(
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <h2 class="h4 fw-bolder">{sport.name}</h2>
+                    <p>name in paragraph</p>
+                        <a class="text-decoration-none" href="#!">
+                            Dummy data
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
+            </div>
+        )
+    })
 
     return(
         <>
-        <MainTextBox data={sport}/>
+        <section class="py-5 border-bottom" id="features">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        {sports && useSports}
+                    </div>
+                </div>
+            </section>
         </>
-    )
+    );
+    function load() {
+        axios.get("https://stats-sports-api.herokuapp.com").then(response => {
+            const sportsData = response.data.data;
+            //console.log(sportsData);
+            setSports(sportsData);
+        }).catch(error => console.error(`Error: ${error}`));
+        /*
+        return(
+            <MainTextBox data={sport}/>
+        )*/
+    }
 }
 export default Content;
--- src/components/MainTextBox.js
+++ src/components/MainTextBox.js
@@ -1,26 +1,33 @@
 import React from "react";
 
-function MainTextBox(data) {
-    
-    const displaySport = (data) => {
-        return(    
-            Object.entries(data).forEach((element) => {
-                //const {sportName} = sport.name;
+function MainTextBox(props) {
+    const displaySport = (props) => {
+        const {sports} = props;
+        return(
+            sports.map(sport => {
+                console.log(sport.id);
                 return(
+                    <section>
+                        <div>
+                        <h2>sport: {sport.name} </h2>
+                    </div>
+                    </section>
+                    
+                    /*
                     <section class="py-5 border-bottom" id="features">
                         <div class="container px-5 my-5">
                             <div class="row gx-5">
                                 <div class="col-lg-4 mb-5 mb-lg-0">
-                                    <h2 class="h4 fw-bolder">hi</h2>
-                                        <p>hi</p>
+                                    <h2 class="h4 fw-bolder">{sport.id}</h2>
+                                        <p>name in paragraph</p>
                                             <a class="text-decoration-none" href="#!">
-                                                Call to action
+                                                Dummy data
                                                 <i class="bi bi-arrow-right"></i>
                                             </a>
                                 </div>
                             </div>
                         </div>
-                    </section>
+                    </section>*/
                 )
             })
         )
@@ -28,7 +35,7 @@ function MainTextBox(data) {
 
     return(
         <>
-            {displaySport(data)}
+            {displaySport(props)}
         </>
     )
     
