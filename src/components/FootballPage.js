a67f17dfad26c4041c483c96e7c5dcd47354db19
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-09-15 19:30:02 -0600

    was able to render all the premier league data from and format it into a table to display

--- src/components/FootballPage.js
+++ src/components/FootballPage.js
@@ -6,9 +6,41 @@ function FootballPage(props) {
 
     const [standings, setStandings] = useState([]);
     useEffect( () => {load();}, []);
+
+    const useStandings = standings.map((teamData) => {
+        return(
+            <tbody>
+                <tr class="col">
+                    <td>{teamData.position}</td>
+                    <td>{teamData.team.name}</td>
+                    <td>{teamData.fields.matches_total}</td>
+                    <td>{teamData.fields.wins_total}</td>
+                    <td>{teamData.fields.draws_total}</td>
+                    <td>{teamData.fields.losses_total}</td>
+                    <td>{teamData.fields.score_diff_formatted_total}</td>
+                    <td>{teamData.fields.points_total}</td>
+                </tr>
+            </tbody>
+        )
+    })
     return(
-        <div>
-            {standings}
+        <div class="ptable">
+            <h1 class="headin">Standings</h1>
+                <table>
+                    <tbody>
+                    <tr class="col">
+                        <th>#</th>
+                        <th>Team</th>
+                        <th>GP</th>
+                        <th>W</th>
+                        <th>D</th>
+                        <th>L</th>
+                        <th>GD</th>
+                        <th>PTS</th>
+                    </tr>
+                    </tbody>
+                    {standings && useStandings}
+                </table>
         </div>
     )
     async function load() {
@@ -18,7 +50,7 @@ function FootballPage(props) {
                 const premierFootballData = response.data.data[0].standings_rows;
                 premierFootballData.sort(compare);
                 //console.log(premierFootballData);
-                //setStandings(premierFootballData);
+                setStandings(premierFootballData);
             }
         }).catch(error => console.error(`Error: ${error}`));
         /* need to fix this where it goes to MainTextBox
