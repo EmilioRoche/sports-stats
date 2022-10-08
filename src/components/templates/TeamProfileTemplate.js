32f906267c2903542e47d6830fe49df31379a086
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2022-10-07 20:11:26 -0600

    fixed styling of team profile

--- src/components/templates/TeamProfileTemplate.js
+++ src/components/templates/TeamProfileTemplate.js
@@ -4,7 +4,7 @@ function TeamProfileTemplate(element){
     const useProfile = element.map((profile) => {
         return (
             <div class="mt-3">
-                <img src={profile.logo} alt="Team-Logo" class="rounded-circle" width="150"/>
+                <img src={profile.logo} alt="Team-Logo" class="rounded" width="150"/>
                 <h4>{profile.team_name}</h4>
                 <p class="text-secondary mb-1">Manager: {profile.manager_id}</p>
                 <p class="text-muted font-size-sm">Venue: {profile.venue_id}</p>
